a28d03670d133e9f1f12eb6071ac6e33ecf8cb01
Dan Erickson <druckson@gmail.com>
2013-12-13 22:36:07 -0800

    add update, draw support to engine, fix entity creation

--- src/engine.lua
+++ src/engine.lua
@@ -2,7 +2,6 @@ local Class = require "lib/hump/class"
 
 local Engine = Class{
     init = function(self)
-        self.entities = {}
         self.systems = {}
         self.nextEntity = 1
     end
@@ -17,16 +16,34 @@ end
 function Engine:createEntity(data)
     local entity = self.nextEntity
     self.nextEntity = self.nextEntity + 1
+
     local entityData = {}
-    for name, system in pairs(systems) do
-        if data[system] ~= nil then
-            system:addEntity(entity, data[system])
+    for name, system in pairs(data) do
+        if self[name] ~= nil then
+            self[name]:addEntity(entity, entityData, system)
         end
     end
 end
 
+function Engine:removeEntity(entity)
+    for name, system in pairs(self.systems) do
+        system:removeEntity(entity)
+    end
+end
+
 function Engine:update(dt)
-    for _, system in pairs(systems) do
+    for _, system in pairs(self.systems) do
+        if system.update ~= nil then
+            system:update(dt)
+        end
+    end
+end
+
+function Engine:draw()
+    for _, system in pairs(self.systems) do
+        if system.draw ~= nil then
+            system:draw()
+        end
     end
 end
 
