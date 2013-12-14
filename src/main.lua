215530ab9215f0fe127472bc25b7d40ff4a3ac83
Dan Erickson <druckson@gmail.com>
2013-12-13 22:37:42 -0800

    add sample level

--- src/main.lua
+++ src/main.lua
@@ -1,16 +1,23 @@
 local Engine = require "engine"
-
+local MapLoader = require "maploader"
+local Graphics = require "systems/graphics"
+local Network = require "systems/graphics"
 
 local engine = Engine()
 
 local graphics = Graphics()
---local graphics = Graphics()
+local network = Network()
+local mapLoader = MapLoader(engine)
 function love.load()
     engine:addSystem("graphics", graphics)
+    engine:addSystem("network", network)
+    mapLoader:loadMap("level1")
 end
 
 function love.update(dt)
+    engine:update(dt)
 end
 
 function love.draw()
+    engine:draw()
 end
