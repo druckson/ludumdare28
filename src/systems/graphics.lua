4ef1a32a94e59e073763e3a2f89ab586f0830d15
Dan Erickson <druckson@gmail.com>
2013-12-13 22:37:03 -0800

    add basic image display

--- src/systems/graphics.lua
+++ src/systems/graphics.lua
@@ -2,10 +2,30 @@ local Class = require "../lib/hump/class"
 
 local Graphics = Class{
     init = function(self)
-        
+        self.entities = {}
+        love.graphics.setBackgroundColor(100, 100, 100)
     end
 }
 
-function Graphics:addEntity(entity, data)
-    
+function Graphics:addEntity(entity, entityData, data)
+    self.entities[entity] = entityData
+
+    entityData.graphics = {
+        width = data.width,
+        height = data.height,
+        image = love.graphics.newImage("assets/images/"..data.image)
+    }
 end
+
+function Graphics:removeEntity(entity)
+    self.entities[entity] = nil
+end
+
+function Graphics:draw()
+    --love.graphics.clear()
+    for _, entity in pairs(self.entities) do
+        love.graphics.draw(entity.graphics.image, 0, 0)
+    end
+end
+
+return Graphics
