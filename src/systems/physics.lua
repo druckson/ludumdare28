e8f598d15028a83c8fe638a1e3e2db05741f7929
Dan Erickson <druckson@gmail.com>
2013-12-14 14:52:20 -0800

    fix marshalling for physics updates

--- src/systems/physics.lua
+++ src/systems/physics.lua
@@ -8,7 +8,7 @@ local Physics = Class{
         self.worldScale = worldScale or 10
         self.world = love.physics.newWorld(0, 0, true)
         love.physics.setMeter(self.worldScale)
-        self.world:setGravity(0, 1)
+        self.world:setGravity(0, 10)
     end
 }
 
@@ -28,21 +28,29 @@ end
 
 function Physics:unmarshall(entity, data)
     local entityData = self.entities[entity]
-    local body = love.physics.newBody(self.world, 
-        entityData.transform.position.x,
-        entityData.transform.position.y, data.type)
 
-    entityData.physics = {
-        body = body,
-        shapes = data.shapes
-    }
+    if not entityData.physics then
+        entityData.physics = {}
+        entityData.physics.body = love.physics.newBody(self.world,
+            entityData.transform.position.x,
+            entityData.transform.position.y, data.type)
 
-    for _, shape in pairs(data.shapes) do
-        local fixture = love.physics.newFixture(entityData.physics.body, newShape(shape), data.density or 1)
+        if data.shapes then
+            for _, shape in pairs(data.shapes) do
+                local fixture = love.physics.newFixture(entityData.physics.body, newShape(shape), data.density or 1)
+                fixture:setRestitution(0.3)
+            end
+        end
+    else
+        --entityData.physics.body:setPosition(
+        --    entityData.transform.position.x,
+        --    entityData.transform.position.y)
     end
+            
+    entityData.physics.body:setAngle(entityData.transform.rotation)
 
     if data.velocity then
-        body:setLinearVelocity(unpack(data.velocity))
+        entityData.physics.body:setLinearVelocity(unpack(data.velocity))
     end
 end
 
